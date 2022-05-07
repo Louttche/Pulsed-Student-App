aa8b2b89c21cfcf8b255d831fab45c8c6c000c93
Kathy <i375722@fhict.nl>
2022-05-07 13:29:00 +0200

    initial commit + Started Fontys Auth

---
+++ PulsedApp/PulsedApp/PulsedApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PulsedApp.Services;
+using PulsedApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PulsedApp.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public string wow = "wowowow";
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            // TODO: Create command that gets auth token and stores it.
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+        
+    }
+}
---
+++ PulsedApp/PulsedApp/PulsedApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using PulsedApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PulsedApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public WebView view;
+
+        // URL properties for Fontys authentication
+        string CLIENT_ID = "i874073-no-production";
+        string REDIRECT_URI = "https://tas.fhict.nl/oob.html";
+        string OAUTH_URL = "https://identity.fhict.nl/connect/authorize";
+        string OAUTH_SCOPE = "fhict fhict_personal";
+
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            view = new WebView();
+
+            Content = new StackLayout
+            {
+                Children = { view }
+            };
+
+            view.Navigated += View_Navigated;
+            view.Source = "http://google.com";
+
+        }
+
+        private void View_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            (sender as WebView).HeightRequest = Height;
+        }
+    }
+}
